8086e4d592a6ccff10b0b8372406ed9406f8c7a5
Fulya Tasliarmut <fulya.tasliarmut@uni-weimar.de>
2018-10-24 10:59:37 +0200

    add TMatrix class

---
+++ vr_assignment_1.py
@@ -0,0 +1,15 @@
+import math
+class TMatrix:
+    def __init__(self, matrix_4x4 == None):
+        if matrix_4x4 == None:
+            # create the identity matrix, if no parameters are given
+            self.matrix_4x4 = [[1, 0, 0, 0],
+                               [0, 1, 0, 0],
+                               [0, 0, 1, 0],
+                               [0, 0, 0, 1]]
+        else:
+            self.matrix_4x4 = matrix_4x4
+
+        def mult(self, other_matrix):
+            pass
+
