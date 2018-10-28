95016ea37a61dfbadc1d2359ecdb4124496c3373
Fulya Tasliarmut <fulya.tasliarmut@uni-weimar.de>
2018-10-28 11:46:57 +0100

    change vector with members to list

--- vr_assignment_1.py
+++ vr_assignment_1.py
@@ -3,6 +3,7 @@ class TMatrix:
     def __init__(self, matrix_4x4=None):
         if matrix_4x4 == None:
             # create a identity matrix, if no parameters are given
+            # the order of the matrices created are in column-major order
             self.matrix_4x4 = [1, 0, 0, 0, 
                                0, 1, 0, 0,
                                0, 0, 1, 0,
@@ -22,24 +23,22 @@ class TMatrix:
                   0, 0, 0, 0]
         A = self.matrix_4x4
         B = other_matrix.matrix_4x4
-        """ result[0] = A[0] * B[0] + A[4] * B[1] + A[8] * B[2] + A[12] * B[3]
-        result[1] = A[1] * B[0] + A[5] * B[1] + A[9] * B[2] + A[13] * B[3]
-        result[2] = A[2] * B[0] + A[6] * B[1] + A[10] * B[2] + A[14] * B[3]
-        result[3] = A[3] * B[0] + A[7] * B[1] + A[11] * B[2] + A[15] * B[3]
-
-        result[4] = A[0] * B[4] + A[4] * B[5] + A[8] * B[6] + A[12] * B[7]
-        result[5] = A[1] * B[4] + A[5] * B[5] + A[9] * B[6] + A[13] * B[7]"""
 
         for i in range(4):
             result[i * 4 + 0] = A[0] * B[i * 4] + A[4] * B[i * 4 + 1] + A[8] * B[i * 4 + 2] + A[12] * B[i * 4 + 3]
             result[i * 4 + 1] = A[1] * B[i * 4] + A[5] * B[i * 4 + 1] + A[9] * B[i * 4 + 2] + A[13] * B[i * 4 + 3]
             result[i * 4 + 2] = A[2] * B[i * 4] + A[6] * B[i * 4 + 1] + A[10] * B[i * 4 + 2] + A[14] * B[i * 4 + 3]
             result[i * 4 + 3] = A[3] * B[i * 4] + A[7] * B[i * 4 + 1] + A[11] * B[i * 4 + 2] + A[15] * B[i * 4 + 3]
-      
         return TMatrix(result)
 
-        def mult_vec(self, vector):
-            pass
+    def mult_vec(self, vector):
+        result = [0, 0, 0, 0]
+        A = self.matrix_4x4
+        b = vector.vector
+        for i in range(4):
+            result[i] = A[i] * b[0] + A[i + 4] * b[1] + A[i + 8] * b[2] + A[i + 12] * b[3]
+        return Vector4(result)
+        
 
     def __str__(self):
         return "[{: 10.4f}, {: 10.4f}, {: 10.4f}, {: 10.4f};\n {: 10.4f}, {: 10.4f}, {: 10.4f}, {: 10.4f};\n {: 10.4f}, {: 10.4f}, {: 10.4f}, {: 10.4f};\n {: 10.4f}, {: 10.4f}, {: 10.4f}, {: 10.4f}]".format(
@@ -50,54 +49,66 @@ class TMatrix:
     
 # Free standing functions
 def make_trans_mat(x, y, z):
-    trans_mat = TMatrix([[1, 0, 0, x], # coloumn 1
-                        [0, 1, 0, y], # coloumn 2
-                        [0, 0, 1, z],
-                        [0, 0, 0, 1]])
+    trans_mat = TMatrix([1, 0, 0, x, 0, 1, 0, y,0, 0, 1, z, 0, 0, 0, 1])
     return trans_mat
 
 def make_rot_mat(degree, axis):
     rad = math.radians(degree)
     if axis == 'x':
-        rot_mat_x = TMatrix([[1.0, 0.0, 0.0, 0.0],
-                            [0.0, math.cos(rad), math.sin(rad), 0.0],
-                            [0.0, -math.sin(rad), math.cos(rad), 0.0],
-                            [0.0, 0.0, 0.0, 1.0]])
+        rot_mat_x = TMatrix([1.0, 0.0, 0.0, 0.0,
+                             0.0, math.cos(rad), math.sin(rad), 0.0,
+                             0.0, -math.sin(rad), math.cos(rad), 0.0,
+                             0.0, 0.0, 0.0, 1.0])
         return rot_mat_x
     elif axis == 'y':
-        rot_mat_y = TMatrix([[math.cos(rad), 0.0, -math.sin(rad), 0.0],
-                            [0.0, 1.0, 0.0, 0.0],
-                            [math.sin(rad), 0.0, math.cos(rad), 0.0],
-                            [0.0, 0.0, 0.0, 1.0]])
+        rot_mat_y = TMatrix([math.cos(rad), 0.0, -math.sin(rad), 0.0,
+                             0.0, 1.0, 0.0, 0.0,
+                             math.sin(rad), 0.0, math.cos(rad), 0.0,
+                             0.0, 0.0, 0.0, 1.0])
         return rot_mat_y
     elif axis == 'z':
-        rot_mat_z = TMatrix([[math.cos(rad), math.sin(rad), 0, 0],
-                            [-math.sin(rad), math.cos(rad), 0,0],
-                            [0, 0, 1, 0],
-                            [0, 0, 0, 1]])
+        rot_mat_z = TMatrix([math.cos(rad), math.sin(rad), 0.0, 0.0,
+                             -math.sin(rad), math.cos(rad), 0.0, 0.0,
+                             0.0, 0.0, 1.0, 0.0,
+                             0.0, 0.0, 0.0, 1.0])
         return rot_mat_z
 
 def make_scale_mat(sx, sy, sz):
-    scale_mat = TMatrix([[sx, 0, 0, 0],
-                        [0, sy, 0, 0],
-                        [0, 0, sz, 0],
-                        [0, 0, 0, 1]])
+    scale_mat = TMatrix([sx, 0, 0, 0,
+                         0, sy, 0, 0,
+                         0, 0, sz, 0,
+                         0, 0, 0, 1])
     return scale_mat
 
 class Vector4:
-    def __init__(self, x=0, y=0, z=0):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, vector):
+        if vector is None:
+            self.vector = [0, 0, 0, 0]
+        else:
+            self.vector = vector
 
+    def __str__(self):
+        return "[{: 10.4f}\n {: 10.4f}\n {: 10.4f}\n {: 10.4f}]".format(self.vector[0], self.vector[1], self.vector[2], self.vector[3])
+    
+
+def euclidean_distance(point1, point2):
+    x_diff = (point2.vector[0] - point1.vector[0])**2
+    y_diff = (point2.vector[1] - point1.vector[1])**2
+    z_diff = (point2.vector[2] - point1.vector[2])**2
+    w_diff = (point2.vector[3] - point1.vector[3])**2
+    distance = math.sqrt((x_diff + y_diff + z_diff + w_diff))
+    return distance
 # Testing
-#print("-----Exercise 1.1-----")
-A = TMatrix([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
-B = TMatrix()
+print("-----Exercise 1.1-----")
+#A = TMatrix([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+#B = TMatrix()
+A = TMatrix([1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 16])
+B = TMatrix([1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15, 8, 16])
 print(A)
 print(B)
 
-print(A.mult(B))
+print("A * B:\n" + str(A.mult(B)))
+
 # print("-----Exercise 1.2-----")
 # print("This is your translation matrix")
 # print(make_trans_mat(1, 2, 3))
@@ -108,3 +119,6 @@ print(A.mult(B))
 # print("This is your scaling matrix")
 # print(make_scale_mat(1, 2, 3))
 
+print(euclidean_distance(Vector4([2, 4, 6, 2]), Vector4([0, 0, 0, 1])))
+b = Vector4([1, 2, 3, 1])
+print("A * b:\n" + str(A.mult_vec(b)))
